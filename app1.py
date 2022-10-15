ad673c2370d13d8e0e71961277b5c94f32829a1c
Rawda-Yousry <rawda.ahmed02@eng-st.cu.edu.eg>
2022-10-15 02:57:51 +0200

    Slidebar_sample

--- app1.py
+++ app1.py
@@ -1,29 +1,33 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+from scipy import signal
 import matplotlib.pyplot as plt
 import mpld3
 import streamlit.components.v1 as components
 import csv
 
 with open('static/test.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    count = 0 
+    reader = csv.reader(csvfile) 
     arr1 = []
     arr2 = []
-    fig = plt.figure() 
     for row in reader:
         arr1.append(row[0])
-        arr2.append(row[1])
-        if count > 10:
-            break
-    arr1.remove(arr1[0])
-    arr2.remove(arr2[0])
+        arr2.append(row[1])    
     arr1Up = [float(x) for x in arr1]
     arr2Up = [float(x) for x in arr2]
+    st.title('ECG signal')
+    fig = plt.figure()
+    plt.xlim([0,1])
+    plt.xlabel("time")
+    plt.ylabel("yaxis")
+    plt.ylim([-1,2])
     plt.plot(arr1Up,arr2Up)
     fig_html = mpld3.fig_to_html(fig)
     components.html(fig_html, height=600)
+    
     st.pyplot(fig)
+
 
 
 
---
+++ appTry.py
@@ -0,0 +1,50 @@
+import numpy as np
+import plotly.graph_objects as go
+import streamlit as st
+from scipy import signal
+from matplotlib import pyplot as plt
+from streamlit_option_menu import option_menu
+import streamlit.components.v1 as components
+import mpld3
+
+with st.sidebar:
+    selected = option_menu(
+        menu_title ="main menu",
+        options =['New','Edit']
+    )
+
+sampleRate = st.slider('Sample Size', 0, 200, 25)
+
+f = 20 # freq (Hz)
+t = np.linspace(0, 0.5, 200) #time steps
+x1 = np.sin(2 * np.pi * f * t) # sine wave 
+s_rate = sampleRate # Hz. Here the sampling frequency is less than the requirement of sampling theorem
+
+
+T = 1 / s_rate 
+n = np.arange(0, 0.5/ T) 
+nT = n * T
+x2 = np.sin(2 * np.pi * f * nT) # Since for sampling t = nT.
+fig= plt.figure(figsize=(9,5))
+plt.subplots_adjust(bottom=0.1,
+                    top=1)
+
+# plt.suptitle("Sampling a Sine Wave of Fmax=20Hz with fs=35Hz", fontsize=20)
+plt.subplot(211)
+plt.plot(t, x1,label='SineWave of frequency 20 Hz')
+plt.xlabel('time', fontsize=15)
+plt.ylabel('Amplitude', fontsize=15)
+
+plt.subplot(212)
+plt.plot(nT, x2, 'ro', label='Sample marks after resampling at fs= ' + str(sampleRate))
+plt.plot(nT, x2, 'g-', label='Reconstructed Sine Wave')
+plt.legend(fontsize=10, loc='upper right')
+
+plt.xlabel('time', fontsize=15)
+plt.ylabel('Amplitude', fontsize=15)
+plt.legend(fontsize=10, loc='upper right')
+fig_html = mpld3.fig_to_html(fig)
+
+plt.tight_layout()
+
+components.html(fig_html, height=600)
