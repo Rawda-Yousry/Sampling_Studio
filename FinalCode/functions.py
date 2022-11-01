72b46501845a3bb98a7fa00aa3f63892e9e7f4bf
AmeeraMOhammed <87406476+AmeeraMOhammed@users.noreply.github.com>
2022-11-01 17:18:42 +0200

    final submission

--- FinalCode/functions.py
+++ FinalCode/functions.py
@@ -1,3 +1,4 @@
+from random import sample
 import streamlit as st
 import pandas as pd
 import mpld3
@@ -7,35 +8,56 @@ from io import StringIO
 import numpy as np
 import plotly.graph_objects as go
 import math
-# from signal_studio_app import *
+from scipy.signal import find_peaks
 
-# fsample = st.slider("Fs",1,300)
-time = np.linspace(0,3,1000)
+time = np.linspace(0,1,1500)
 class Functions:
-    # st.session_state.sig = pd.DataFrame({})
-    numberSignalsAdded=-1
+    
+    numberSignalsAdded = -1
     ADDED_FREQUENCES=[]
     ADDED_AMPLITUDES=[]
-    # ADDED_PHASES=[]
+    UploadAmplitude = np.zeros(1500)
     ADDED_SIGNALS=[]
-    amplitude_SUM = np.zeros(1000)  # Sum of AMPLITUDE of signals
-    Current_amplitude=np.zeros(1000)
-    
+    amplitude_SUM = np.zeros(1500)  # Sum of AMPLITUDE of signals
+    Current_amplitude = np.zeros(1500)
+    defaultcose =[]
 
 
 
-def ADD_SIGNAL(added_magnitude,added_frequency):
-    new_y_amplitude = np.zeros(1000)  # Array for saving sin signals values
-    for i in range(1000): 
-         new_y_amplitude[i] = (added_magnitude * (math.sin((2 * np.pi * added_frequency * time[i])))) 
+#  Function that is used to add multiple signals by ucosg frequencies and amplitudes entered by users. 
+# - Function parameters : Frequency - Amplitude of new signal that we need to add
+# - Function Return : Time - Amplitude that we need to plot   
+# - For loop to store the new amplitude that we need to plot in the array 
+# - updating lists to add new frequncies and amplitudes in the lists 
+# - Add the new amplitude to the previouse amplitudes
+def ADD_SIGNAL(added_magnitude ,added_frequency, phase):
+    ##Signal##
+    new_y_amplitude = np.zeros(1500)  # Array for saving cos signals values
+    for i in range(1500): 
+        new_y_amplitude[i] = (added_magnitude * (math.cos(((2 * np.pi * added_frequency * time[i]) + phase)))) 
     #updating lists
     Functions.numberSignalsAdded+=1
     Functions.ADDED_FREQUENCES.append(added_frequency)
     Functions.ADDED_AMPLITUDES.append(added_magnitude)
     Functions.ADDED_SIGNALS.append(new_y_amplitude)
-    Functions.Current_amplitude=np.add(Functions.Current_amplitude,new_y_amplitude)
-    return time, Functions.Current_amplitude
+    Functions.Current_amplitude = np.add(Functions.Current_amplitude,new_y_amplitude)
+    Functions.UploadAmplitude =np.add(Functions.UploadAmplitude,new_y_amplitude)
+    return Functions.Current_amplitude,Functions.UploadAmplitude
 
+
+
+# Function that return the maximum frequency this is done by entering the list of frequencies then, iterating on it and getting the max. value.
+def maxFrequency():
+    if len(Functions.ADDED_FREQUENCES) == 0:
+        return 1
+    return np.max(Functions.ADDED_FREQUENCES)
+
+
+
+
+# Function clean the graph from gargbe to start from zero.
+# - Function parameters: None 
+# - Function Return : None
 def Clean_intialize():
     #number of signals added
     Functions.numberSignalsAdded=-1
@@ -44,72 +66,104 @@ def Clean_intialize():
     Functions.ADDED_AMPLITUDES=[]
     Functions.ADDED_PHASES=[]
     Functions.ADDED_SIGNALS=[]
-    Functions.amplitude_SUM = np.zeros(1000)  # Sum of AMPLITUDE of signals
-    Functions.Current_amplitude=np.zeros(1000)
-    Functions.Current_amplitude=np.zeros(1000)
+    Functions.amplitude_SUM = np.zeros(1500)  # Sum of AMPLITUDE of signals
+    Functions.Current_amplitude=np.zeros(1500)
 
 
 
-def DELETE_SIGNAL(index_todelete): 
-    if(Functions.numberSignalsAdded==0):
-        Clean_intialize()
-        return time, Functions.Current_amplitude
+# Function to delete signal from the list and plot the signals.
+# - Function parameters : index of signal that we need to remove
+# - Function Return : Time - New Amplitude after deleting 
+# - Delete the signal by its index in the list 
+# - Check if there is signals or not :
+#  -> if there is no signals so intialize the graph 
+#  -> if there is signals so  : 1) subtract the removed amplitude from the current amplitude
+#                               2) pop freqency from its list
+#                               3) pop amplitude from its list
+#                               4) pop signal from its list
+def DELETE_SIGNAL(index_todelete):
+    if(Functions.numberSignalsAdded== -1):
+            Clean_intialize()
     else:
-        print('here')
-        Functions.Current_amplitude=np.subtract(Functions.Current_amplitude,Functions.ADDED_SIGNALS[index_todelete]  )
+        Functions.Current_amplitude=np.subtract(Functions.Current_amplitude,Functions.ADDED_SIGNALS[index_todelete])
         Functions.ADDED_FREQUENCES.pop(index_todelete)
         Functions.ADDED_AMPLITUDES.pop(index_todelete)
-        # Functions.ADDED_PHASES.pop(index_todelete)
         Functions.ADDED_SIGNALS.pop(index_todelete)
-        Functions.numberSignalsAdded-=1
-        return time, Functions.Current_amplitude
+        Functions.numberSignalsAdded -= 1
+       
 
 
+# Function that read csv file when we upload the file.
+#   - Function parameters : csv file 
+#   - Function Return : Time - Added amplitudes   
+#   1) Upload file 
+#   2) Read file as bytes and store in (byte_data)
+#   3) Convert bytes of file to string based on IO and store as string in (stringio)
+#   4) Read the file as string in(string_data)
+#   5) Read file as data fram (df)
+#   6) First column at the csv  (Time)
+#   7) Second column at csv (Amplitude)
+#   😎 Add the amplitudes to each other 
 def readCsv(uploaded_file):
-    if uploaded_file is not None:
-        # To read file as bytes:
-        bytes_data = uploaded_file.getvalue()
-        # To convert to a string based IO:
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        # To read file as string:
-        string_data = stringio.read()
-        df =  pd.read_csv(uploaded_file)
-        x = df.iloc[:,1]
-        y = df.iloc[:,2]
-        # st.write(x,y)
-        # interpolatedSignal = df.iloc[:,3]
-        Functions.Current_amplitude=np.add(Functions.Current_amplitude,y)
-        return x,Functions.Current_amplitude
+    # if uploaded_file is not None:
+    # To convert to a string based IO:
+    df =  pd.read_csv(uploaded_file)
+    Time = df.iloc[:,1]
+    Amplitude = df.iloc[:,2]
+    sample_interpolated=df.iloc[:,3]
+    Functions.UploadAmplitude=np.add(Functions.UploadAmplitude,Amplitude)
+    return Time,Functions.UploadAmplitude,sample_interpolated
 
 
-def sampling(fsample,t,sin):
-    samp_frq=fsample
+
+# Function that sample the signal by entering different sampling frequencies.
+# - Function parameters : Sampling Freqency(fsample) - Time (t) - Signal (cos)
+# - Function Return : Sample Time - Sample Amplitude
+# 1) Get timer range by subtract min time from max time 
+# 2) Get the sample rate by its equation 
+# 3) Get sample time and sample amplitude 
+def sampling(fsample,t,cos):
     time_range=(max(t)-min(t))
-    samp_rate=int((len(t)/time_range)/((fsample)))
-    samp_time=t[::samp_rate]
-    samp_amp= sin[::samp_rate]
-    return samp_time,samp_amp
+    sampling_rate = int((len(t)/time_range)/fsample)
+    sampling_time=t[::sampling_rate] 
+    sampling_amplitude = cos[::sampling_rate]
+    return sampling_time,sampling_amplitude
 
 
-def sinc_interp(fsample,t,sin):
-    samp_time,samp_amp=sampling(fsample,t,sin)
-    samp_time=np.array(samp_time)
-    samp_amp=np.array(samp_amp)
-    # st.write(samp_time)
-    time_matrix= np.resize(t,(len(samp_time),len(t)))
+
+
+# Function used to make cosc interploation (recounstracting) on the signal from it’s sampling points.
+# - Function parameters : Sampling Freqency(fsample) - Time (t) - Signal (cos)
+# - Function Return : Reconstructed signal - Sampling time - Sampling ammplitude
+# 1) First we make sample on the signal 
+# 2) Store sampling time and sampling amplitude in numpy array 
+# 3) Then we make resize to the time and store in matrix
+def sinc_interp(fsample,t,cos ):
+    samplingTime,samplingAmplitude=sampling(fsample,t,cos )
+    samplingTime=np.array( samplingTime)
+    samplingAmplitude=np.array(samplingAmplitude)
+    time_matrix= np.resize(t,(len( samplingTime),len(t)))
     print(time_matrix.size)
-    print(samp_time)
-    fsample=np.array(fsample)
+    print( samplingTime)
     t=np.array(t)
-    sin=np.array(sin)
-    k = (time_matrix.T - samp_time)/(samp_time[1]-samp_time[0])
-    resulted_matrix = samp_amp* np.sinc(k)
+    cos=np.array(cos)
+    shiftingFactor = (time_matrix.T -  samplingTime)/( samplingTime[1]- samplingTime[0])
+    resulted_matrix = samplingAmplitude* np.sinc(shiftingFactor)
     reconstucted_seg = np.sum(resulted_matrix, axis=1)
-    return  reconstucted_seg,samp_time,samp_amp
+    return  reconstucted_seg, samplingTime,samplingAmplitude
 
-def noise(signal,snrRatio):
+
+
+# Function that generate noise on signal.
+# - Function parameters : Siganl - SNR (Signal to Noise Ratio)
+# - Function Return : Noised signal 
+# 1) Get the signal power 2 
+# 2) Use numpy mean function to get the mean of power array 
+# 3) Substract signal to noise ratio from average power 
+# 4) Use numpy random normal function to get the noise that we would add on the signal 
+# 5) Add noise on the original signal
+def noise(signal,snr_db):
     power = signal**2
-    snr_db = 10 * np.log10(snrRatio)
     signal_avg_power=np.mean(power)
     signal_avg_power_db=10 * np.log10(signal_avg_power)
     noise_db=signal_avg_power_db - snr_db
@@ -119,28 +173,29 @@ def noise(signal,snrRatio):
     noise_signal = signal+noise
     return noise_signal
 
-def save(sin):
-    signalAndInterpolation = {"x-axis":time, "y-axis":Functions.Current_amplitude,
+
+
+
+# Function used to save the signal as csv file on yout PC.
+#  - Function Parameters : Signal (cos)
+# - Function Return : None
+# 1) Get the x-axis (Time) and y-axis (Amplitude)
+# 2) Use DataFrame to convert this signal to csv file 
+def save(cos,interpolatedSignal,maxFrequency):
+    signalAndInterpolation = {"x-axis":time, "y-axis":cos ,'sample-interpolated':interpolatedSignal,'maxFrequency':maxFrequency
                                }
     df= pd.DataFrame(signalAndInterpolation)
 
     st.session_state.sig = df
     
-
-
     @st.experimental_memo
     def convert_df(df):
         df.to_csv(index = False).encode('utf-8')
-
-    st.download_button(
-    "Press to Download",
+    col1, col2, col3= st.columns([1,1,1])
+    col2.download_button(
+    "Download",
     st.session_state.sig.to_csv(),
     "file.csv",
     "text/csv",
     key='download-csv'
     )
-
-
-
-
-
